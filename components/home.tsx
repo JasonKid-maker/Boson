c13915f530acc2546db79662ac32d844872a7ce3
DaShanTongYin <m17612011020@outlook.com>
2025-04-16 00:15:41 +0800

    Update home.tsx

--- components/home.tsx
+++ components/home.tsx
@@ -125,6 +125,7 @@ const Home = ({
           onClick={() => {
             router.push("/blog/1d6c9329-68e0-8170-aa5c-fdcf890693c4"); // 替换为你的目标链接
           }}
+          role="button"
         >
           
         </div>
