124ce27f0a844cf89d884de10227aeab9b18e6d0
DaShanTongYin <m17612011020@outlook.com>
2025-04-18 10:34:24 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -12,6 +12,6 @@ export const siteConfig = {
 
 export const notionBlogConfig = {
   //blogParentId: "1d6c9329-68e0-81a3-81b4-f94fcc8f0105",
-  blogParentId: "1d8c9329-68e0-8054-81c1-e183ef24caf0",
+  blogParentId: "1d8c9329-68e0-8054-81c1-e183ef24caf1",
   // https://meowing-numeric-b46.notion.site/1d8c932968e0805481c1e183ef24caf1?pvs=4
 };
